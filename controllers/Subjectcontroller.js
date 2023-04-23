06a47a89c36e0dfe66e500ecd1acdd6401955000
vmane2873@gmail.com <vmane2873@gmail.com>
2023-04-23 11:54:07 +0530

    nice

--- controllers/Subjectcontroller.js
+++ controllers/Subjectcontroller.js
@@ -5,7 +5,7 @@ const getSubjectsByClass = async (req, res) => {
   const { Class } = req.params;
 
   try {
-    const subjects = await Subject.find({ Class });
+    const subjects = await Subject.find({ Class: Class.toString() });
 
     if (subjects.length === 0) {
       return res.status(404).json({ error: 'No subjects found' });
@@ -16,6 +16,7 @@ const getSubjectsByClass = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
 
 
 const getSubjects = async (req, res) => {
